55e497889fdf59dfe9705a7ec3f6c115ab102d2a
adam.zhang <yiyiadam@yahoo.com>
2018-04-18 19:36:09 +0800

    submit hw08

---
+++ hw08/hw08.py
@@ -0,0 +1,28 @@
+
+def deep_len(lst):
+    """Returns the deep length of the list.
+
+    >>> deep_len([1, 2, 3])     # normal list
+    3
+    >>> x = [1, [2, 3], 4]      # deep list
+    >>> deep_len(x)
+    4
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> deep_len(x)
+    6
+    >>> x = []
+    >>> for i in range(100):
+    ...     x = [x] + [i]       # very deep list
+    ...
+    >>> deep_len(x)
+    100
+    """
+    "*** YOUR CODE HERE ***"
+    if not type(lst) == list:
+        return 1
+    else:
+        total = 0
+        for l in lst:
+            total += deep_len(l)
+        return total
+            
