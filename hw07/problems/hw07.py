7bf29168a090939a9950071d9d52c15a5ec3eef0
adam.zhang <yiyiadam@yahoo.com>
2018-04-18 19:01:22 +0800

    sumbit hw07

--- hw07/problems/hw07.py
+++ hw07/problems/hw07.py
@@ -88,7 +88,36 @@ class VendingMachine:
     'Here is your soda.'
     """
     "*** YOUR CODE HERE ***"
-
+    def __init__(self,goods,price):
+        self.goods = goods
+        self.stock = 0
+        self.price = price 
+        self.balance = 0
+    def vend(self):
+        if self.stock <= 0:
+            return 'Machine is out of stock.'
+        elif self.balance < self.price:
+            return 'You must deposit $' + str(self.price - self.balance) +' more.'
+        elif self.balance == self.price:
+            self.stock -=1
+            self.balance = 0
+            return 'Here is your ' + self.goods +   '.'
+        else:
+            change =  self.balance - self.price
+            self.stock -=1
+            self.balance = 0
+            return 'Here is your ' + self.goods +  ' and $' + str(change) + ' change.'            
+    def restock(self,n):
+        self.stock +=n
+        return   'Current ' + self.goods +' stock: ' + str(self.stock)
+    def deposit(self,money):
+        self.balance += money
+        if self.stock <= 0:
+            return 'Machine is out of stock. Here is your $' + str(self.balance) + '.'
+        else:
+            return 'Current balance: $' + str(self.balance) 
+import re
+rule=re.compile(r'[^a-zA-z]')
 class MissManners:
     """A container class that only forward messages that say please.
 
@@ -128,3 +157,12 @@ class MissManners:
         if not message.startswith(magic_word):
             return 'You must learn to say please first.'
         "*** YOUR CODE HERE ***"
+        order = message[7:]
+        if hasattr(self.obj, order):
+            return getattr(self.obj, order)(*args)
+        else:
+            return 'Thanks for asking, but I know not how to {0}.'.format(order)
+        
+
+
+            
