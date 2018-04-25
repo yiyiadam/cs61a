07f0943201f0c58ac8bdbdbdf900e0c3069c0a7c
adam.zhang <yiyiadam@yahoo.com>
2018-04-25 21:08:17 +0800

    submit scm code of hw09

--- hw09/hw09.scm
+++ hw09/hw09.scm
@@ -2,44 +2,59 @@
   (cdr (cdr s)))
 
 (define (cadr s)
-  'YOUR-CODE-HERE
-  nil
+  (car (cdr s))
 )
 
 (define (caddr s)
-  'YOUR-CODE-HERE
-  nil
+  (car (cdr (cdr s)))
 )
 
 (define (sign x)
-  'YOUR-CODE-HERE
-  nil
+  (cond
+    ((< x 0) -1)
+    ((= x 0) 0)
+    ((> x 0) 1)
+    )
 )
-
 (define (square x) (* x x))
 
 (define (pow b n)
-  'YOUR-CODE-HERE
+  (cond
+	((= n 0) 1)
+	((even? n) (square (pow b (/ n 2))))
+	((* b (square (pow b (/ (- n 1) 2)))))
+  )
 )
 
 (define (ordered? s)
-  'YOUR-CODE-HERE
-  nil
+  (cond
+    ((null? s) #t)
+    ((null? (cdr s)) #t)
+    ((> (car s) (cadr s)) #f)
+    (else (ordered? (cdr s)))
+  )
 )
 
 (define (nodots s)
-  'YOUR-CODE-HERE
-  nil
-)
+  (cond
+    ((null? s) s)
+    ((pair? s)
+      (cond 
+        ((pair? (car s))  (cons (nodots (car s))  (nodots (cdr s))))
+        (else (cons (car s) (nodots (cdr s))))
+      ))
+    (else (cons s '()))
 
+  )
+)
 ; Sets as sorted lists
 
 (define (empty? s) (null? s))
 
 (define (contains? s v)
     (cond ((empty? s) false)
-          'YOUR-CODE-HERE
-          (else nil) ; replace this line
+          ((= (car s) v) #t)
+          (else (contains? (cdr s) v)) ; replace this line
           ))
 
 ; Equivalent Python code, for your reference:
@@ -59,14 +74,16 @@
 
 (define (add s v)
     (cond ((empty? s) (list v))
-          'YOUR-CODE-HERE
-          (else nil) ; replace this line
+          ((< v (car s)) (cons v s))
+          ((> v (car s)) (cons (car s) (add (cdr s) v)))
+          (else s)
           ))
 
 (define (intersect s t)
     (cond ((or (empty? s) (empty? t)) nil)
-          'YOUR-CODE-HERE
-          (else nil) ; replace this line
+            ((= (car s) (car t)) (cons (car s) (intersect (cdr s) (cdr t))))
+            ((< (car s) (car t)) (intersect (cdr s) t ))
+            (else  (intersect s (cdr t)))
           ))
 
 ; Equivalent Python code, for your reference:
@@ -86,8 +103,9 @@
 (define (union s t)
     (cond ((empty? s) t)
           ((empty? t) s)
-          'YOUR-CODE-HERE
-          (else nil) ; replace this line
+          ((= (car s) (car t)) (cons (car s) (union (cdr s) (cdr t))))
+		  ((< (car s) (car t)) (cons (car s) (union (cdr s) t)))
+		  (else (cons (car t) (union s (cdr t))))
           ))
 
 ; Q9 - Survey
