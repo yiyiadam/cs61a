687c2407c574f7767c59ebd5caeabd413ff477e9
adam.zhang <yiyiadam@yahoo.com>
2018-04-28 16:59:17 +0800

    submit lab09

--- lab09/lab09.scm
+++ lab09/lab09.scm
@@ -2,7 +2,9 @@
 
 ; Q2
 (define (over-or-under x y)
-  'YOUR-CODE-HERE
+  (cond ((> x y) 1)
+        ((< x y)-1) 
+        (else 0))
 )
 
 ;;; Tests
@@ -15,7 +17,11 @@
 
 ; Q3
 (define (filter f lst)
-  'YOUR-CODE-HERE
+  (if (null? lst) lst
+      (if (f (car lst)) (cons (car lst) (filter f (cdr lst)))
+          (filter f (cdr lst))
+      )
+  )
 )
 
 ;;; Tests
@@ -26,7 +32,7 @@
 
 ; Q4
 (define (make-adder num)
-  'YOUR-CODE-HERE
+  (lambda (x) (+ num x))
 )
 
 ;;; Tests
