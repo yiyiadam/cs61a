ed8fc0403f8172512fd1307491f928afba4cdee7
adam.zhang <yiyiadam@yahoo.com>
2018-05-17 23:09:47 +0800

    submit problem 13

--- scheme/scheme.py
+++ scheme/scheme.py
@@ -267,6 +267,7 @@ def do_lambda_form(expressions, env):
 def do_if_form(expressions, env):
     """Evaluate an if form."""
     check_form(expressions, 2, 3)
+    print(expressions)
     if scheme_truep(scheme_eval(expressions.first, env)):
         return scheme_eval(expressions.second.first, env)
     elif len(expressions) == 3:
@@ -275,13 +276,28 @@ def do_if_form(expressions, env):
 def do_and_form(expressions, env):
     """Evaluate a short-circuited and form."""
     # BEGIN PROBLEM 13
-    "*** REPLACE THIS LINE ***"
-    # END PROBLEM 13
+    if expressions == nil:
+        return True
+    elif len(expressions) == 1:
+        return scheme_eval(expressions.first,env)
+    else:
+        if scheme_truep(scheme_eval(expressions.first,env)) == False:
+            return False
+        else:
+            return do_and_form(expressions.second,env)
 
 def do_or_form(expressions, env):
     """Evaluate a short-circuited or form."""
     # BEGIN PROBLEM 13
-    "*** REPLACE THIS LINE ***"
+    if expressions == nil:
+        return False
+    elif len(expressions) == 1:
+        return scheme_eval(expressions.first,env)
+    else:
+        if scheme_truep(scheme_eval(expressions.first,env)):
+            return scheme_eval(expressions.first,env)
+        else:
+            return do_or_form(expressions.second, env)
     # END PROBLEM 13
 
 def do_cond_form(expressions, env):
