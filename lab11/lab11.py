d2488f0285f72906f3f2fc2829065b7d8159ffad
adam.zhang <yiyiadam@yahoo.com>
2018-05-21 19:56:54 +0800

    submit lab11

--- lab11/lab11.py
+++ lab11/lab11.py
@@ -11,6 +11,9 @@ def countdown(n):
     0
     """
     "*** YOUR CODE HERE ***"
+    return [ n - i for i in range(n+1)]
+    
+
 
 def trap(s, k):
     """Return a generator that yields the first K values in iterable S,
@@ -28,6 +31,13 @@ def trap(s, k):
     """
     assert len(s) >= k
     "*** YOUR CODE HERE ***"
+    i = 0
+    while True:
+        if i < k:
+            yield s[i]
+            i +=1
+        else:
+            raise ValueError("Out of range")
 
 def repeated(t, k):
     """Return the first value in iterable T that appears K times in a row.
@@ -42,6 +52,22 @@ def repeated(t, k):
     """
     assert k > 1
     "*** YOUR CODE HERE ***"
+    itr_first = iter(t)
+    count = 1
+    
+    first = next(itr_first)
+    while True:
+        try:
+            temp = next(itr_first)
+            if temp == first:
+                count +=1
+                if count >=k:
+                    return temp
+            else:
+                first = temp
+                count =1
+        except ValueError:
+            return None
 
 def hailstone(n):
     """
@@ -57,3 +83,14 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    while n != 1:
+        yield n
+        if n%2 == 0:
+            n = n//2
+        else:
+            n = 3*n + 1  
+        
+        if n==1:
+            yield n
+        else:
+            pass 
