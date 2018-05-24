3e0f29c75c4c326638aaa1f434d303063b9c7893
adam.zhang <yiyiadam@yahoo.com>
2018-05-24 08:50:06 +0800

    submit lab12

--- lab12/lab12.sql
+++ lab12/lab12.sql
@@ -2,16 +2,18 @@
 .read fa16data.sql
 
 CREATE TABLE obedience AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven,denero FROM students ;
 
 CREATE TABLE smallest_int AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT time,smallest FROM students WHERE smallest > 8 ORDER BY smallest LIMIT 20;
 
 CREATE TABLE greatstudents AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.date, a.number, a.pet, a.color, b.color  FROM students AS a, sp16students AS b WHERE a.date = b.date AND a.number=b.number AND a.pet = b.pet;
 
 CREATE TABLE sevens AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select s.seven from students as s, checkboxes as c
+    where s.time = c.time and s.number = 7 and c."7" = "True";
 
 CREATE TABLE matchmaker AS
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+  select a.pet, a.song, a.color, b.color from students as a, students as b
+    where a.pet = b.pet and a.song = b.song and a.time < b.time;
